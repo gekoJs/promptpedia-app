1c4dc1cde2c328c4ed0d02cdc7e569a0175a9e23
gekoJs <daniroa.js@gmail.com>
2023-05-24 09:58:01 -0400

    test

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,13 +1,14 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
-    console.log(prompts)
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return NextResponse.json(prompts);
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 404 });
+    return NextResponse.error();
+    // return new Response("Failed to fetch all prompts", { status: 404 });
   }
 };
